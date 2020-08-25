c6e969e20d218e0bee32727395e7eeb731e8eec7
magmal <magdalena.malczewska@yahoo.pl>
2020-08-25 22:00:42 +0200

    Commit 25.08, added layout for Pick Table Fragment (view that will be shown on fist start of app and when you want to check/add another table.

---
+++ app/src/main/java/com/mm/planzajec_pracainzynierska/FirstStartOnlyActivity.java
@@ -0,0 +1,14 @@
+package com.mm.planzajec_pracainzynierska;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstStartOnlyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_start_only);
+    }
+}
