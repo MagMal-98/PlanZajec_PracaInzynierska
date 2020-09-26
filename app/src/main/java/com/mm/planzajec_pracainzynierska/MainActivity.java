9a6759574a893e74f790d8590d6ba4f79c3d493d
magmal <magdalena.malczewska@yahoo.pl>
2020-09-26 21:14:25 +0200

    added activity layout for daily schelude and picking plan after first install

--- app/src/main/java/com/mm/planzajec_pracainzynierska/MainActivity.java
+++ app/src/main/java/com/mm/planzajec_pracainzynierska/MainActivity.java
@@ -2,13 +2,53 @@ package com.mm.planzajec_pracainzynierska;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            Intent intent = new Intent(MainActivity.this, WeeklyScheduleActivity.class);
+            startActivity(intent);
+        }
+
+        });
+
+//
+
+        //sec version 2check
+        Boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+                .getBoolean("isFirstRun", true);
+
+        if (isFirstRun) {
+            //show sign up activity
+            startActivity(new Intent(MainActivity.this, FirstStartOnlyActivity.class));
+            Toast.makeText(MainActivity.this, "Run only once", Toast.LENGTH_LONG)
+                    .show();
+            getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit()
+                    .putBoolean("isFirstRun", false).apply();
+        }
+
+
+
+
+
+
     }
 }
